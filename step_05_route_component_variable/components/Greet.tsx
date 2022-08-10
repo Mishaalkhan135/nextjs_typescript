ba2aba17347b53870d528ea1be13d9ff09d25c6b
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2022-08-11 00:20:49 +0500

    Completed the step 05

---
+++ step_05_route_component_variable/components/Greet.tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+import type { NextComponentType, NextPageContext } from "next";
+
+const Name: NextComponentType<NextPageContext> = () => {
+	const { query } = useRouter();
+	console.log(query);
+	return <h1>Hello, {query.name}</h1>;
+};
+
+export default Name;
---
+++ step_05_route_component_variable/pages/greet/[name].tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Greet from "../../components/Greet";
+
+const name: NextPage = () => {
+	return (
+		<div>
+			<Greet></Greet>
+		</div>
+	);
+};
+
+export default name;
