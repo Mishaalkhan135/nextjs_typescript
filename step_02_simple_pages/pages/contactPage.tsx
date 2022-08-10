9299e9a0c70ba899daed0a73de3a4cfb660b3ce5
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2022-08-10 21:24:21 +0500

    Completed the step 02

---
+++ step_02_simple_pages/pages/contactPage.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+
+const contactPage = () => {
+	return (
+		<div>
+			<h1>This is the contact page </h1>
+			<ul>
+				<li>Email: mishaalkhan135@gmail.com</li>
+				<li>Facebook : Mishaal kahn</li>
+				<li>Instagram: Mishaal khan</li>
+			</ul>
+		</div>
+	);
+};
+
+export default contactPage;
