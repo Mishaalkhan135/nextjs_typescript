241a607a1eff9172a252b68466993aef5095aa7f
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2022-08-15 23:57:33 +0500

    Added the types

--- step_23_global_state_management_context_api/pages/index.tsx
+++ step_23_global_state_management_context_api/pages/index.tsx
@@ -1,7 +1,8 @@
 import ProductCard from "../component/Productcard";
 import { products } from "../data/items";
+import type { NextPage } from "next";
 
-const Home = () => {
+const Home: NextPage = () => {
 	return (
 		<div className='grid grid-cols-4 gap-4'>
 			{products.map((product) => (
